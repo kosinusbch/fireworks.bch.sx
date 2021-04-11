6e9d42c987f9bd1f4bb255dae71c097812fcab24
kos <kos@bch.sx>
2021-04-11 12:42:25 -0700

    move to bitsocket-sa old format

--- bitsocket.js
+++ bitsocket.js
@@ -9,15 +9,15 @@ bitsocket.onmessage = function(e) {
         // Since bch has very few tx's per minute, we'll send one firework per transaction output (3 max).
         // The transaction itself is stored in event.data[0]
 
-        for (var i = 0; i < event.data.out.length; i++) {
+        for (var i = 0; i < event.data[0].out.length; i++) {
             var wmax = (screen.width * 0.8);
             var wmin = (screen.width * 0.2);
-            if(event.data.out[i] && event.data.out[i].address) {
+            if(event.data[0].out[i] && event.data[0].out[i].e && event.data[0].out[i].e.a) {
                 if(i < 1) newFireworkSeed(Math.floor(Math.random() * (wmax - wmin + 1) + wmin), (screen.height - (document.getElementById('stream').offsetHeight * 1.37)));
                 var el = document.getElementById('list'), ch = document.createElement('div');
                 ch.setAttribute('class', 'new_tx');
                 ch.setAttribute('data-timestamp', Date.now());
-                ch.innerHTML = '<b><a href="https://explorer.bitcoin.com/bch/tx/' + event.data.tx.hash + '" target="_blank">*</a> ' + event.data.out[i].address.substring(0, 4) + '...'  + event.data.out[i].address.substring(25) + '</b> received <b>' + (event.data.out[i].value * 0.00000001).toFixed(8) + ' BCH</b>';
+                ch.innerHTML = '<b><a href="https://explorer.bitcoin.com/bch/tx/' + event.data[0].tx.h + '" target="_blank">*</a> ' + event.data[0].out[i].e.a.substring(0, 4) + '...'  + event.data[0].out[i].e.a.substring(34) + '</b> received <b>' + (event.data[0].out[i].e.v * 0.00000001).toFixed(8) + ' BCH</b>';
                 el.insertBefore(ch, el.firstChild);
             }
         }
